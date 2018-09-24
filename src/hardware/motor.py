468ccfd6274db42acefd807b4b55440240d1421e
obin1000 <robinvonk789@gmail.com>
2018-09-24 12:04:16 +0200

    Add comments and bugfixes

--- src/hardware/motor.py
+++ src/hardware/motor.py
@@ -13,60 +13,76 @@ class Motor:
     richtingr = 0
 
     def __init__(self):
+        """
+        Create a bus connection over I2C and sets the speed at 0
+        """
         self.bus = smbus.SMBus(1)  # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1) <- found on internet, hope it makes sense to you
-        self.bus.write_i2c_block_data(self.ADDRESS, self.OFFSET, self.MOTORST)  # Sets the motor to stop, just to be safe :P
+        self.left(0)
+        self.right(0)
 
-    #  stop the motors and stop the I2c bus connection
     def stop(self) -> bool:
+        """
+        Stop the motors and stop the I2c bus connection
+        :return: returns a bool based on success
+        """
         self.left(0)
         self.right(0)
         self.bus.close()
         return True
 
-    # change the speed of the left wheels range: -255 to 255
-    def left(self, v: int) -> bool:
-        if v > -256 and v < 256:
-            if v == 0:
+    def left(self, speed: int) -> bool:
+        """
+        Speed and direction of the left wheels
+        :param speed: Range from -255 to 255
+        :return: returns a bool based on success
+        """
+        if 256 < speed > -256:
+            if speed == 0:
                 self.speedl = 0
                 self.richtingl = 0
-            if v > 0:
-                self.speedl = v
+            if speed > 0:
+                self.speedl = speed
                 self.richtingl = 1
-            if v < 0:
-                self.speedl = -v
+            if speed < 0:
+                self.speedl = -speed
                 self.richtingl = 2
         else:
             return False
         self._send_data()
         return True
 
-    # change the speed of the right wheels range: -255 to 255
-    def right(self, v: int) -> bool:
-        if v > -256 and v < 256:
-            if v == 0:
+    def right(self, speed: int) -> bool:
+        """
+        Speed and direction of the right wheels
+        :param speed: Range from -255 to 255
+        :return: returns a bool based on success
+        """
+        if 256 < speed > -256:
+            if speed == 0:
                 self.speedr = 0
                 self.richtingr = 0
-            if v > 0:
-                self.speedr = v
+            if speed > 0:
+                self.speedr = speed
                 self.richtingr = 1
-            if v < 0:
-                self.speedr = -v
+            if speed < 0:
+                self.speedr = -speed
                 self.richtingr = 2
         else:
             return False
         self._send_data()
         return True
 
-    #  send help
     def status(self) -> dict:
         pass
 
-    #  send help
     def get_speed(self) -> int:
         pass
 
-    #  generate an array of data for the motorcontroller and send it over the I2C bus
     def _send_data(self) -> bool:
+        """
+        generate an array of data for the motorcontroller and send it over the I2C bus
+        :return: returns a bool based on success
+        """
         motor_data = [7, 3, self.speedl, self.richtingl, 3, self.speedr, self.richtingr]
         self.bus.write_i2c_block_data(self.ADDRESS, self.OFFSET, motor_data)
         return True
