9ec3cf1d3098d581ae867bdadee7789ecd978f89
david de ROW rover <david@rover-ROW>
2018-09-24 14:36:17 +0000

    smbus2 is alivemake run!

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import cv2
 import platform
 from src.common.log import *
+from src.hardware.motor import *
 
 
 def main():
     log.info("hello, world!")
     log.debug("python version: %s", platform.python_version())
     log.debug("OpenCV version: %s", cv2.__version__)
+#    mot = Motor()
+#    mot.left(0)
 
 
 if __name__ == "__main__":
