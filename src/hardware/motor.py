ec4441eaf801e40ab7775ea06b25ff159eea6012
Noeël Moeskops <noeel.moeskops@hva.nl>
2018-09-25 15:19:56 +0200

    motor aangepast zodat hij werkt met de api en exceptions toegevoegt

--- src/hardware/motor.py
+++ src/hardware/motor.py
@@ -1,8 +1,8 @@
 from smbus2 import *
+from src.common.log import *
 
 
 class Motor:
-
     OFFSET = 0  # offset in the array
     ADDRESS = 0x32  # I2c address of the motorcontroller
     # speed from 4 to 250
@@ -16,9 +16,19 @@ class Motor:
         """
         Create a bus connection over I2C and sets the speed at 0
         """
-        self.bus = SMBus(1)  # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1) <- found on internet, hope it makes sense to you
+        try:
+            self.bus = SMBus(
+                1)  # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1) <- found on internet, hope it makes sense to you
+        except FileNotFoundError as er:
+            log.error("%s, normal if in fake env.", er)
         self.left(0)
         self.right(0)
+
+    def __del__(self):
+        """
+        closes the i2c bus.
+        """
+        self.stop()
 
     def stop(self) -> bool:
         """
@@ -28,6 +38,7 @@ class Motor:
         self.left(0)
         self.right(0)
         self.bus.close()
+        # TODO dit returnt altijd true, moet natuurlijk op basis van de uitkomst. @robin1
         return True
 
     def left(self, speed: int) -> bool:
@@ -48,6 +59,7 @@ class Motor:
                 self.richtingl = 2
         else:
             raise ValueError("{0} is not in the range of -255 to 255".format(speed))
+        # TODO functie checkt niet of send_data() succesvol is. @robin1
         self._send_data()
         return True
 
@@ -69,24 +81,48 @@ class Motor:
                 self.richtingr = 2
         else:
             raise ValueError("{0} is not in the range of -255 to 255".format(speed))
+        # TODO functie checkt niet of send_data() succesvol is. @robin1
         self._send_data()
         return True
 
     def status(self) -> dict:
+        # TODO implementeren @robin1
+        # wat is dit eigenlijk?
         pass
 
     def get_speed(self) -> int:
+
+        # TODO implementeren @robin1
         pass
+
+    def get_value_left(self) -> int:
+        """
+        Get left speed value
+        :return (int): value
+        """
+        # TODO documentatie
+        return self.speedl
+
+    def get_value_right(self):
+        """
+        Get right speed value
+        :return (int): value
+        """
+        return self.speedr
 
     def _send_data(self) -> bool:
         """
         generate an array of data for the motorcontroller and send it over the I2C bus
         :return: returns a bool based on success
         """
-        try:
-            motor_data = [7, 3, self.speedl, self.richtingl, 3, self.speedr, self.richtingr]
-            self.bus.write_i2c_block_data(self.ADDRESS, self.OFFSET, motor_data)
-        except IOError as e:
-            print("I/O error({0}): {1}".format(e.errno, e.strerror))
-            return False
-        return True
+        if config["Motor"].getboolean("simulate_motor") == False:
+            try:
+                motor_data = [7, 3, self.speedl, self.richtingl, 3, self.speedr, self.richtingr]
+                self.bus.write_i2c_block_data(self.ADDRESS, self.OFFSET, motor_data)
+            except IOError as e:
+                print("I/O error({0}): {1}".format(e.errno, e.strerror))
+                return False
+            else:
+                return True
+        else:
+            return True
