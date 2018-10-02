5258e83ca08ad157fb771aed15ea1df32b7e3804
obin1000 <robinvonk789@gmail.com>
2018-10-02 15:46:03 +0200

    Added controller and keyboard support to the webpage

--- web/static/assets/index.js
+++ web/static/assets/index.js
@@ -6,6 +6,95 @@ window.requestAnimationFrame = window.requestAnimationFrame
 	|| window.mozRequestAnimationFrame
 	|| function(callback) { window.setTimeout(callback, 1000 / 60);};
 
+var inputObject = {
+    "up": {
+        "key": ["KEY_W", "UP_ARROW"],
+        "pad": ["DPAD_UP", "RIGHT_TRIGGER"]
+    },
+    "down": {
+        "key": ["KEY_S", "DOWN_ARROW"],
+        "pad": ["DPAD_DOWN", "LEFT_TRIGGER"],
+        "axes": ["LEFT_Y"]
+    },
+    "left": {
+        "key": ["KEY_A", "LEFT_ARROW"],
+        "pad": ["DPAD_LEFT"]
+    },
+    "right": {
+        "key": ["KEY_D", "RIGHT_ARROW"],
+        "pad": ["DPAD_RIGHT"],
+        "axes": ["LEFT_X"]
+    }
+};
+// What controller to lisen to
+var controllerIndex = 0;
+var topSpeed = 255;
+// our input objects
+var up = new Input.Input(inputObject.up, controllerIndex);
+var down = new Input.Input(inputObject.down, controllerIndex);
+var left = new Input.Input(inputObject.left, controllerIndex);
+var right = new Input.Input(inputObject.right, controllerIndex);
+
+// up
+up.press = function() {
+	l = topSpeed * this.value;
+	r = topSpeed * this.value;
+	updateRL();
+};
+up.release = function() {
+    if (down.isUp) {
+    	l = 0;
+    	r = 0;
+        updateRL();
+    }
+    else down.press();
+};
+
+// down
+down.press = function() {
+    l = -topSpeed * this.value;
+    r = -topSpeed * this.value;
+    updateRL();
+};
+down.release = function() {
+    if (down.isUp) {
+        l = 0;
+        r = 0;
+        updateRL();
+    }
+    else down.press();
+};
+
+// left
+left.press = function() {
+    l = -topSpeed * this.value;
+    r = topSpeed * this.value;
+    updateRL();
+};
+left.release = function() {
+    if (down.isUp) {
+        l = 0;
+        r = 0;
+        updateRL();
+    }
+    else down.press();
+};
+
+// right
+right.press = function() {
+    l = topSpeed * this.value;
+    r = -topSpeed * this.value;
+    updateRL();
+};
+right.release = function() {
+    if (down.isUp) {
+        l = 0;
+        r = 0;
+        updateRL();
+    }
+    else down.press();
+};
+
 function callLoop(delta){
 	var le = l.toFixed(0);
 	var ri = r.toFixed(0);
@@ -19,6 +108,11 @@ function callLoop(delta){
 	    }
 	});
 	window.setTimeout(callLoop, 500);
+}
+
+function updateRL(){
+    $("#l")[0].innerHTML = l.toFixed(1);
+    $("#r")[0].innerHTML = r.toFixed(1);
 }
 
 function clamp(num, min, max) {
@@ -41,8 +135,7 @@ function move(movedX, movedY){
 	r = clamp(r, -255, 255);
 	l = clamp(l, -255, 255);
 
-	$("#l")[0].innerHTML = l.toFixed(1);
-	$("#r")[0].innerHTML = r.toFixed(1);
+	updateRL();
 }
 
 $(".joystick")[0].addEventListener('touchstart', function(e) {
