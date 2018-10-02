c4fc3f3a4813d01d4f2f5d2937ae52c14012c366
Robin de Jong <robin@destrandhoeve.nl>
2018-10-02 10:52:00 +0200

    Fixed ajax parameters

--- web/static/assets/index.js
+++ web/static/assets/index.js
@@ -1,24 +1,25 @@
+var x, y, l = 0.0, r = 0.0;
+var multiplier = 3.5;
+
 window.requestAnimationFrame = window.requestAnimationFrame
 	|| window.webkitRequestAnimationFrame
 	|| window.mozRequestAnimationFrame
 	|| function(callback) { window.setTimeout(callback, 1000 / 60);};
 
 function callLoop(delta){
+	var le = l.toFixed(0);
+	var ri = r.toFixed(0);
 	$.ajax({
 	    url: '/api/motor',
 	    method: 'PUT',
 	    data: {
-		key: 1234,
-	    	left: l,
-	    	right: r
-	    },
-	    contentType: 'application/json'
+			key: 1234,
+	    	left: le,
+	    	right: ri
+	    }
 	});
-	requestAnimationFrame(callLoop);
+	window.setTimeout(callLoop, 500);
 }
-
-var x, y, l, r;
-var multiplier = 3.5;
 
 function clamp(num, min, max) {
   return num <= min ? min : num >= max ? max : num;
@@ -62,7 +63,7 @@ $(".joystick")[0].addEventListener('touchmove', function(e) {
 
 
 $(".joystick").on('touchend touchcancel', function() {
-	move(0, 0);
+	move(0.0, 0.0);
 
 	$(".joystick").removeClass("active");
 });
